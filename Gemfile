1b59bd069667f660145f159f69b34fdd8d655d6b
Eric Lacaille <elacaille0@gmail.com>
2016-05-10 18:14:09 +0200

    Login feature added

--- Gemfile
+++ Gemfile
@@ -24,7 +24,7 @@ gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0', group: :doc
 
 # Use ActiveModel has_secure_password
-# gem 'bcrypt', '~> 3.1.7'
+gem 'bcrypt', '~> 3.1.7'
 
 # Use Unicorn as the app server
 # gem 'unicorn'
@@ -37,6 +37,8 @@ group :development, :test do
   gem 'byebug'
   gem 'binding_of_caller'
   gem 'better_errors'
+  gem 'faker'
+  gem 'redcarpet'
 end
 
 group :development do
---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,46 @@
+class ArticlesController < ApplicationController
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @articles = Article.all
+  end
+
+  def show
+    @markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, extensions = {})
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    @article.save
+    redirect_to article_path(@article)
+  end
+
+  def edit
+  end
+
+  def update
+    # on reccupere l'article en sortie de form grace a before action ci-dessus
+    @article.update(article_params)
+    redirect_to article_path(@article)
+  end
+
+  def destroy
+    @article.destroy
+    redirect_to articles_path
+  end
+
+  private
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :content)
+  end
+end
+
